bcac9e7fb1a75ee09ad32799083e34a9c5a62e8a
Junseo <monstazo911@gmail.com>
2024-12-05 21:41:30 +0900

    fix: 안드로이드 다크 모두 방지

--- android/app/src/main/java/com/cheering/MainApplication.kt
+++ android/app/src/main/java/com/cheering/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.microsoft.codepush.react.CodePush;
+import androidx.appcompat.app.AppCompatDelegate;
 
 class MainApplication : Application(), ReactApplication {
 
@@ -36,6 +37,7 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
